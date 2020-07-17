68c1628c56f33d75175e271d56c0654c1f1647de
nasir.ahmad3073 <nasirnawruzi@gmail.com>
2020-07-17 21:01:29 +0700

    Basic app for rectangle

--- app.js
+++ app.js
@@ -4,21 +4,21 @@ const rect = {
 };
 
 function solveRect(l,b) {
-    console.log("rectangle with the value l = " + l + " and b = " + b);
+    console.log("rectangle with the value l = " + l + " and b = " + b)
     if (l === 0 || b === 0) {
     	console.log('Remember put correct value for l and b, dimensions should be greater then zero. ');
     }
 
    
     else {
-	    console.log("The area of the rectangle is " + rect.area(l,b));
-	    console.log("The perimeter of the rectangle is " + rect.perimeter(l,b));
+	    console.log("The area of the rectangle is " + rect.area(l,b))
+	    console.log("The perimeter of the rectangle is " + rect.perimeter(l,b))
     }
 }
 
-solveRect(2,4);
-solveRect(3,5);
-solveRect(0,0);
+solveRect(2,4)
+solveRect(3,5)
+solveRect(0,0)
 
 
 
